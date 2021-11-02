6992a17d5d4cdf419cea9d1d5397c8f8258cadf4
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2021-11-02 09:17:42 -0500

    Add basic struc of threads, missing thread pool

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -11,67 +11,52 @@ import java.lang.Thread;
 public class EchoServer {
     public static final int portNumber = 6013;
 
-    public static void main(String[] args) {
-        try {
-            // Start listening on the specified port
-            ServerSocket socket = new ServerSocket(portNumber);
-
-            // Run forever, which is common for server style services
-            while (true) {
-                // Wait until someone connects, thereby requesting a date
-                Socket client = socket.accept();
-                System.out.println("Got a request!");
-
-                // Helpful guide here:
-                // https://stackoverflow.com/questions/1830698/what-is-inputstream-output-stream-why-and-when-do-we-use-them
-                // Object that gets input from the client
-                InputStream clientInput = client.getInputStream();
-                // Object that sends output to the client
-                OutputStream clientOutput = client.getOutputStream();
-
-                Thread thread = new ClientHandler(client, clientInput, clientOutput);
-
-                thread.start();
-                
-
-                
-            }
-            // *Very* minimal error handling.
-        } catch (IOException ioe) {
-            System.out.println("We caught an unexpected exception");
-            System.err.println(ioe);
-        }
-    }   
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EchoServer server = new EchoServer();
+		server.start();
 }
 
- class ClientHandler extends Thread {
-     final InputStream clientInput;
-     final OutputStream clientOutput;
-     final Socket socket;
+private void start() throws IOException, InterruptedException {
 
-    public ClientHandler(Socket socket, InputStream clientInput, OutputStream clientOutput) {
-        this.socket = socket;
-        this.clientInput = clientInput;
-        this.clientOutput = clientOutput;
+    ServerSocket serverSocket = new ServerSocket(portNumber);
+
+    while (true) {
+
+        Socket socket = serverSocket.accept();
+        clientHandler newClientConnection = new clientHandler(socket);
+
+        Thread newClientThread = new Thread(newClientConnection);
+
+        newClientThread.start();
     }
-        
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            // Buffer size of client input
-            byte[] bufferSize = new byte[1024];
-            int bytesRead;
-            // Read data sent by client socket
-            while ((bytesRead = clientInput.read(bufferSize)) != -1) {
-                // Send data back to client console
-                clientOutput.write(bufferSize, 0, bytesRead);
-                // Clear buffer
-                clientOutput.flush();
+}
+
+public class clientHandler implements Runnable {
+
+    private Socket socket;
+
+    public clientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            InputStream fromClientStream = socket.getInputStream();
+            OutputStream toClientStream = socket.getOutputStream();
+
+            int clientInput = fromClientStream.read();
+
+            while (clientInput != -1) {
+                toClientStream.write(clientInput);
+                clientInput = fromClientStream.read();
             }
 
-            // TODO: Close the socket via client
-            // Close the client socket since we're done.
-            socket.close();
-        }
-        
+            toClientStream.flush();
+            socket.shutdownOutput();
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } 
     }
+}
+} 
